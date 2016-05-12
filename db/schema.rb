62e67f057ee573292dd4f3d5da126f2643182584
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-12 17:46:28 -0400

    performed rake db:rollback because the sightings model I created didn't have the attributes I wanted it to have.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512214021) do
-
-  create_table "sightings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
