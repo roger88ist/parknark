e00dd1ff0fd076d5d035265c9182a65b1a76adee
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-12 18:01:03 -0400

    set root to 'sightings#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'sightings#index'
   resources :interest_locations
-  resources :sightings
   resources :sightings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
