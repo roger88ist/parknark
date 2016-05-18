f0b2fbce4bdef5a6f35d393b96b3d625d21aa1ac
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-18 17:31:49 -0400

    Cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-readyselector
 //= require_tree .
-// $(document).ready(function() { 
-
+function ready() { 
+  
+  initGeolocation();
+  // initMap();
 
   function initGeolocation() {
     if( navigator.geolocation ) {
@@ -25,7 +28,6 @@
     else {
      alert("Sorry, your browser does not support geolocation services.");
     }
-
   }    
 
   function success(position){
@@ -34,11 +36,41 @@
   }
 
   function fail() {
-// Could not obtain location
+  // Could not obtain location
   }
 
 
-  var map;
+  // var infoWindow = new google.maps.InfoWindow({map: map});
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //       var pos = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //       lat: 
+  //       };
+
+  //     // infoWindow.setPosition(pos);
+  //     // infoWindow.setContent('YOU ARE HERE');
+  //     map.setCenter(pos);
+  //     map.setZoom(15);
+  //     map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+  //     }, function() {
+  //     handleLocationError(true, infoWindow, map.getCenter());
+  //   });
+  // } else {
+  //   // Browser doesn't support Geolocation
+  //   handleLocationError(false, infoWindow, map.getCenter());
+  // }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+  }
+}
+
+var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     // center: final[0],
@@ -72,35 +104,4 @@
     }
   }
 
-
-  // var infoWindow = new google.maps.InfoWindow({map: map});
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //       var pos = {
-  //       lat: position.coords.latitude,
-  //       lng: position.coords.longitude,
-  //       lat: 
-  //       };
-
-  //     // infoWindow.setPosition(pos);
-  //     // infoWindow.setContent('YOU ARE HERE');
-  //     map.setCenter(pos);
-  //     map.setZoom(15);
-  //     map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
-  //     }, function() {
-  //     handleLocationError(true, infoWindow, map.getCenter());
-  //   });
-  // } else {
-  //   // Browser doesn't support Geolocation
-  //   handleLocationError(false, infoWindow, map.getCenter());
-  // }
-
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-  }
-
-$(document).on("ready page:load", initGeolocation);
+$(document).on("ready page:load", ready);
